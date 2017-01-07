2658be1364d041a0711d884d75455049ba9fb84d
bigrobsf <rjconner@mac.com>
2017-01-07 13:56:12 -0800

    added transparency filter

--- public/js/show-image.js
+++ public/js/show-image.js
@@ -22,6 +22,7 @@ function openImgInCanvas(imageURL) {
     edgeDetect();
     pixelize();
     adjBrightness();
+    adjAlpha();
 
     document.getElementById('origCanvas').addEventListener('click', function() {
       window.open(canvas.toDataURL('image/jpeg'), '_blank');
@@ -139,7 +140,6 @@ function blurImage(blurRadius = 2, listener = false) {
   canvas.width = imgW;
   canvas.height = imgH;
 
-  console.log(blurRadius);
   context.filter = 'blur(' + blurRadius + 'px)';
 
   context.drawImage(imageObj, 0, 0);
@@ -321,6 +321,61 @@ function adjBrightness(adjustment = 30, listener = false) {
   }
 
   document.getElementById('dl-brightness').addEventListener('click', function() {
+    this.href = canvas.toDataURL('image/jpeg');
+  }, false);
+
+  return context.canvas.toDataURL('data/jpeg', 1.0);
+}
+
+//==============================================================================
+// change transparency of image
+function adjAlpha(alpha = 125, listener = false) {
+  console.log(alpha);
+
+  let imageObj = document.getElementById('origCanvas');
+
+  let canvas = document.getElementById('alphaCanvas');
+  let context = canvas.getContext('2d');
+
+  let imgW = imageObj.width;
+  let imgH = imageObj.height;
+  canvas.width = imgW;
+  canvas.height = imgH;
+
+  context.drawImage(imageObj, 0, 0);
+
+  let imgPixels = context.getImageData(0, 0, imgW, imgH);
+
+  for (let y = 0; y < imgPixels.height; y++) {
+    for (let x = 0; x < imgPixels.width; x++) {
+      let i = (y * 4) * imgPixels.width + x * 4;
+
+      // if (x < 40 && y < 40) console.log(imgPixels.data[i], imgPixels.data[i + 1], imgPixels.data[i + 2], imgPixels.data[i + 3]);
+
+      imgPixels.data[i + 3] = alpha;
+    }
+  }
+
+  context.putImageData(imgPixels, 0, 0, 0, 0, imgPixels.width, imgPixels.height);
+
+  let rangeInput = document.getElementById('alpha');
+
+  if (listener === false) {
+    document.getElementById('alphaCanvas').addEventListener('click', function() {
+      window.open(canvas.toDataURL('image/jpeg'), '_blank');
+    });
+
+    rangeInput.addEventListener('change', function() {
+      document.getElementById('alpha').textContent = rangeInput.value;
+      alpha = Number(rangeInput.value);
+
+      adjAlpha(alpha, listener);
+    });
+
+    listener = true;
+  }
+
+  document.getElementById('dl-alpha').addEventListener('click', function() {
     this.href = canvas.toDataURL('image/jpeg');
   }, false);
 
